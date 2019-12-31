5bef2046200d07fc4cf01f07c22288ac89959fe7
Jose David Perez Hurtado <jose_dph@hotmail.com>
2019-12-31 10:09:08 -0400

    Arreglos y cambios para ingresar los valores

--- SimulationProject/MainWindow.xaml.cs
+++ SimulationProject/MainWindow.xaml.cs
@@ -30,6 +30,8 @@ namespace SimulationProject
             InitializeComponent();
 
             DataContext = viewModel = new MainViewModels();
+
+            viewModel.ChangeValuesCommand.Execute(null);
         }
 
 
